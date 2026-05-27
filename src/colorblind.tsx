c8f1fcd7c57aa1d8ae4c5aca93cca5c1c274d99c
Abel de Bruijn <abeldebruijn@hotmail.com>
2026-05-27 11:45:49 +0200

    Remove stray SVG from colorblind preview markdown

--- src/colorblind.tsx
+++ src/colorblind.tsx
@@ -420,11 +420,6 @@ function getDetailMarkdown(result: ColorBlindnessResult): string {
   return [
     `![Colour blindness preview](${getPreviewImageUrl(result)})`,
     "",
-    `<svg height="100" width="100" xmlns="http://www.w3.org/2000/svg">
-      <circle r="45" cx="50" cy="50" stroke="green" stroke-width="3" fill="red" />
-      Sorry, your browser does not support inline SVG.
-    </svg>`,
-    "",
     `# ${getColorBlindnessTypeLabel(result.type)}`,
     "",
     `Source: \`${result.colour}\``,
